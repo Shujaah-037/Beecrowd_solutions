d5aacb74006bc59f31f6a1f0260783901f2fb390
Shujat Ali <113830666+Shujaah-037@users.noreply.github.com>
2023-01-14 18:40:52 +0530

    Update 2630.py

--- 2630.py
+++ 2630.py
@@ -11,3 +11,30 @@ for i in range(T):
       print(f"Caso #3: {int(.30*R+.59*G+.11*B)}")
       
       #note: I am having 10% Wrong ans:
+    
+    #you can use the code below:
+    t = int(input())
+I = 1
+while t:
+    str = input()
+    a = list(map(float, input().split()))
+    if str[0] == 'e':
+        a[0] = (a[0]*30)/100
+        a[1] = (a[1]*59)/100
+        a[2] = (a[2]*11)/100
+        a[0] = a[0] + a[1] + a[2]
+        f = int(a[0])
+        print("Caso #{}: {}".format(I, f))
+    elif len(str) == 4:
+        m = int((a[0] + a[1] + a[2]) / 3)
+        print("Caso #{}: {}".format(I, m))
+    else:
+        a.sort()
+        if str[1] == 'i':
+            f = int(a[0])
+            print("Caso #{}: {}".format(I, f))
+        else:
+            f = int(a[2])
+            print("Caso #{}: {}".format(I, f))
+    t -= 1
+    I += 1
