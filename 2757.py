c59929a9f5525c77367ae3409c87e9b60099dde9
Shujat Ali <113830666+Shujaah-037@users.noreply.github.com>
2023-01-15 08:09:01 +0530

    Create 2757.py

---
+++ 2757.py
@@ -0,0 +1,8 @@
+#beecrowd | 2757
+#Input and Output of Integers
+a, b, c = map(int, input().split())
+
+print("A = {}, B = {}, C = {}".format(a, b, c))
+print("A = {:10}, B = {:10}, C = {:10}".format(a, b, c))
+print("A = {:010}, B = {:010}, C = {:010}".format(a, b, c))
+print("A = {:-10}, B = {:-10}, C = {:-10}".format(a, b, c))
